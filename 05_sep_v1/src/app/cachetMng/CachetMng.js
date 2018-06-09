697c21a9185b901e05351ea08f29de1a60db9505
webj2ee <1696627335@qq.com>
2018-06-09 10:43:04 +0800

    增加UI复杂度、数据联动；

--- 05_sep_v1/src/app/cachetMng/CachetMng.js
+++ 05_sep_v1/src/app/cachetMng/CachetMng.js
@@ -21,7 +21,7 @@ export default class CachetMng extends React.Component {
 
     componentDidMount(){
         this.props.dispatch({
-            type: "cachetMng/FETCH_ZLBXX_REQUESTED"
+            type: "cachetMng/FETCH_CACHET_TYPE_LIST_REQUESTED"
         });
     }
 
@@ -39,16 +39,31 @@ export default class CachetMng extends React.Component {
 
     render(){
 
-        const {zlbxxList, loading} = this.props.cachetMng;
+        const {cachetTypeList, cachetList, cachetImageUrl, cachetLoca, loading} = this.props.cachetMng;
 
         return (
             <Hlayout>
                 {/* 章类别信息 */}
                 <Panel width={370}>
                     <Grid  ref={(ele)=>{this.dwCachetTypeInfo = ele} }
-                           dataSource={zlbxxList}
+                           dataSource={cachetTypeList}
                            loading={loading}
                            rowKey="empno"
+                           onRow={(record) => {
+                               return {
+                                   onClick: () => { // 点击行
+                                       this.props.dispatch({
+                                           type: "cachetMng/FETCH_CACHET_LIST_REQUESTED",
+                                           payload: record.zlbbh
+                                       });
+
+                                       this.props.dispatch({
+                                           type: "cachetMng/FETCH_CACHET_LOCA_REQUESTED",
+                                           payload: record.zlbbh
+                                       });
+                                   },
+                               };
+                           }}
                            columns={[{
                                title: '章类别编号',
                                dataIndex: 'zlbbh',
@@ -67,13 +82,63 @@ export default class CachetMng extends React.Component {
                     {/* 章信息 */}
                     <Tab defaultActiveKey="cachetInfo">
                         <TabPage tab="章信息" key="cachetInfo">
-                            章信息
+
+
+                            <Hlayout>
+
+                                <Panel>
+                                    <Grid  ref={(ele)=>{this.dwCachetInfo = ele} }
+                                           dataSource={cachetList}
+                                           loading={loading}
+                                           rowKey="mbid"
+                                           onRow={(record) => {
+                                               return {
+                                                   onClick: () => { // 点击行
+                                                       this.props.dispatch({
+                                                           type: "cachetMng/SET_CACHET_IMAGE_URL_REQUESTED",
+                                                           payload: record.zbh,
+                                                       });
+                                                   },
+                                               };
+                                           }}
+                                           columns={[{
+                                               title: '章编号',
+                                               dataIndex: 'zbh',
+                                           },{
+                                               title: '章名',
+                                               dataIndex: 'zmc',
+                                           }, {
+                                               title: '数字签章名称',
+                                               dataIndex: 'sigzbh',
+                                           },{
+                                               title: '章高度',
+                                               dataIndex: 'zgd',
+                                           }, {
+                                               title: '章宽度',
+                                               dataIndex: 'zkd',
+                                           },{
+                                               title: '章类别',
+                                               dataIndex: 'zlbbh',
+                                           }]}
+                                    />
+                                </Panel>
+
+                                <Panel>
+                                    <img src={cachetImageUrl} style={{width: 100, height:100}}/>
+                                </Panel>
+                            </Hlayout>
+
+
+
+                            <Button onClick={()=>this.handleAddZlbxx()}>新增</Button>
+                            <Button onClick={()=>this.handleEditZlbxx()}>修改</Button>
+                            <Button onClick={()=>this.handleDelZlbxx()}>删除</Button>
                         </TabPage>
                         <TabPage tab="章所在模板" key="cachetLoca">
                             <Grid  ref={(ele)=>{this.dwTempInfor = ele} }
-                                   dataSource={[]}
+                                   dataSource={cachetLoca}
                                    loading={loading}
-                                   rowKey="empno"
+                                   rowKey="mbid"
                                    columns={[{
                                        title: '格式名称',
                                        dataIndex: 'mbmc',
--- 05_sep_v1/src/app/cachetMng/modelCachetMng.js
+++ 05_sep_v1/src/app/cachetMng/modelCachetMng.js
@@ -11,7 +11,10 @@ const namespace = "cachetMng";
 // InitialState
 //
 const initialState = {
-    zlbxxList: [],
+    cachetTypeList: [],
+    cachetLoca:[],
+    cachetList: [],
+    cachetImageUrl: "",
     loading: false,
 };
 
@@ -23,18 +26,62 @@ function cachetReducer(state = initialState, action) {
     const {type, payload} = action;
 
     switch (type) {
-        case `${namespace}/FETCH_ZLBXX_INPROGRESS`:
+
+        // 查询章类别信息
+        case `${namespace}/FETCH_CACHET_TYPE_LIST_INPROGRESS`:
             return {
                 ...state,
-                zlbxxList: [],
+                cachetTypeList: [],
                 loading: true,
             };
-        case `${namespace}/FETCH_ZLBXX_SUCCESS`:
+        case `${namespace}/FETCH_CACHET_TYPE_LIST_SUCCESS`:
             return {
                 ...state,
-                zlbxxList: payload,
+                cachetTypeList: payload,
                 loading: false,
             };
+
+
+
+        // 查询章类别下的章信息列表
+        case `${namespace}/FETCH_CACHET_LIST_INPROGRESS`:
+            return {
+                ...state,
+                cachetList: [],
+                loading: true,
+            };
+        case `${namespace}/FETCH_CACHET_LIST_SUCCESS`:
+            return {
+                ...state,
+                cachetList: payload,
+                loading: false,
+            };
+
+
+        //根据章类别编号查询所在的模板信息
+        case `${namespace}/FETCH_CACHET_LOCA_INPROGRESS`:
+            return {
+                ...state,
+                cachetLoca: [],
+                loading: true,
+            };
+        case `${namespace}/FETCH_CACHET_LOCA_SUCCESS`:
+            return {
+                ...state,
+                cachetLoca: payload,
+                loading: false,
+            };
+
+
+
+        //设置章图片URL
+        case `${namespace}/SET_CACHET_IMAGE_URL_SUCCESS`:
+            return {
+                ...state,
+                cachetImageUrl: payload,
+            };
+
+
         default:
             return state;
     }
@@ -43,20 +90,85 @@ function cachetReducer(state = initialState, action) {
 /////////////////////////////////////////////////////////////////////////////
 // Sagas
 //
-function* fetchZlbxxWatcher() {
-    yield takeLatest(`${namespace}/FETCH_ZLBXX_REQUESTED`, function*(action) {
+// 查询章类别信息
+function* fetchCachetTypeListWatcher() {
+    yield takeLatest(`${namespace}/FETCH_CACHET_TYPE_LIST_REQUESTED`, function*(action) {
 
-        yield put({type: `${namespace}/FETCH_ZLBXX_INPROGRESS`});
+        yield put({type: `${namespace}/FETCH_CACHET_TYPE_LIST_INPROGRESS`});
+        const list = yield call(request, "/sep/CachetServlet/fetchCachetTypeList");
+        yield put({type: `${namespace}/FETCH_CACHET_TYPE_LIST_SUCCESS`, payload: list});
 
-        const list = yield call(request, "/sep/CachetServlet/fetchZlbxx");
 
-        yield put({type: `${namespace}/FETCH_ZLBXX_SUCCESS`, payload: list});
+        // 做联动查询；章类别信息 ==> 章信息列表
+        //                    ==> 章类别所在模板信息
+        if(list.length){
+            const zlbbh = list[0].zlbbh;
+            yield put({type: `${namespace}/FETCH_CACHET_LOCA_REQUESTED`, payload:zlbbh});
+            yield put({type: `${namespace}/FETCH_CACHET_LIST_REQUESTED`, payload:zlbbh});
+        }
+    });
+}
+
+
+
+
+
+// 查询章类别下的章信息列表
+function* fetchCachetListWatcher() {
+    yield takeLatest(`${namespace}/FETCH_CACHET_LIST_REQUESTED`, function*(action) {
+
+        yield put({type: `${namespace}/FETCH_CACHET_LIST_INPROGRESS`});
+
+        const zlbbh = action.payload;
+        const list = yield call(request, `/sep/CachetServlet/fetchCachetList?zlbbh=${zlbbh}`);
+        yield put({type: `${namespace}/FETCH_CACHET_LIST_SUCCESS`, payload: list});
+
+
+        // 做图片预览数据联动
+        //
+        if(list.length){
+            const zbh = list[0].zbh;
+            yield put({type: `${namespace}/SET_CACHET_IMAGE_URL_REQUESTED`, payload:zbh});
+        }
+
+
+
+
 
     });
 }
+//根据章类别编号查询所在的模板信息
+function* fetchCachetLocaWatcher() {
+    yield takeLatest(`${namespace}/FETCH_CACHET_LOCA_REQUESTED`, function*(action) {
+
+        yield put({type: `${namespace}/FETCH_CACHET_LOCA_INPROGRESS`});
+
+        const zlbbh = action.payload;
+        const list = yield call(request, `/sep/CachetServlet/fetchCachetLoca?zlbbh=${zlbbh}`);
+        yield put({type: `${namespace}/FETCH_CACHET_LOCA_SUCCESS`, payload: list});
+
+    });
+}
+
+function* setCachetImageUrlWatcher() {
+    yield takeLatest(`${namespace}/SET_CACHET_IMAGE_URL_REQUESTED`, function*(action) {
+
+        const cacheImageUrl = `/sep/CachetServlet/fetchCachetImage?zbh=${action.payload}&_=${Math.random()}`;
+
+        yield put({
+            type: `${namespace}/SET_CACHET_IMAGE_URL_SUCCESS`,
+            payload: cacheImageUrl,
+        });
+
+    });
+}
+
 function* cachetSaga(){
     yield all([
-        fetchZlbxxWatcher(),
+        fetchCachetTypeListWatcher(),
+        fetchCachetListWatcher(),
+        fetchCachetLocaWatcher(),
+        setCachetImageUrlWatcher(),
     ]);
 };
 
