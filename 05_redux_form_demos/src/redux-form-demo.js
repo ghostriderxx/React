533e1759142138863c1b28676239ce4c99fc6c55
webj2ee <1696627335@qq.com>
2018-07-02 16:30:47 +0800

    redux-form-demo
    Signed-off-by: webj2ee <1696627335@qq.com>

--- 05_redux_form_demos/src/redux-form-demo.js
+++ 05_redux_form_demos/src/redux-form-demo.js
@@ -6,17 +6,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // ## Redux
-import {createStore, combineReducers} from 'redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import {Provider, connect} from 'react-redux';
 
 // ## Redux-Form
-import { reducer as formReducer, Field, reduxForm } from 'redux-form'
+import {reducer as formReducer, Field, reduxForm} from 'redux-form'
+
+// ## Devtools
+import {composeWithDevTools} from "redux-devtools-extension";
 
 ///////////////////////////////////////////////////////////////////////////////
 // UserInfoForm
 //
 @reduxForm({
-    form: 'contact'
+    form: 'userInfo',
+    onSubmit: (values) => {
+        console.log("userInfo", values);
+    }
 })
 class UserInfoForm extends React.Component {
     constructor(props) {
@@ -24,24 +30,84 @@ class UserInfoForm extends React.Component {
     }
 
     render() {
-        const { handleSubmit } = this.props;
+        const {handleSubmit, reset, resetSection} = this.props;
 
         return (
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="firstName">姓名：</label>
-                    <Field name="firstName" component="input" type="text" />
-                </div>
-                <div>
-                    <label htmlFor="lastName">Last Name</label>
-                    <Field name="lastName" component="input" type="text" />
-                </div>
-                <div>
-                    <label htmlFor="email">邮箱：</label>
-                    <Field name="email" component="input" type="email" />
-                </div>
-                <button type="submit">提交</button>
-            </form>
+            <fieldset>
+                <legend>个人信息</legend>
+                <form onSubmit={handleSubmit}>
+                    姓名：
+                    <Field name="name" component="input" type="text"/>
+                    <br/><br/>
+
+                    年龄：
+                    <Field name="age" component="input" type="number"/>
+                    <br/><br/>
+
+                    性别：
+                    <Field name="gender" component="select">
+                        <option>请选择</option>
+                        <option value="male">男</option>
+                        <option value="female">女</option>
+                    </Field>
+                    <br/><br/>
+
+                    <div>
+                        <button onClick={() => resetSection("name")}>清空[姓名]</button>
+                        <button onClick={() => resetSection("age")}>清空[年龄]</button>
+                        <button onClick={() => resetSection("gender")}>清空[性别]</button>
+                        <button onClick={() => reset()}>清空全部</button>
+                        <button type="submit">提交</button>
+                    </div>
+                </form>
+            </fieldset>
+        );
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// BookInfoForm
+//
+@reduxForm({
+    form: 'bookInfo',
+    onSubmit: (values) => {
+        console.log("bookInfo", values);
+    }
+})
+class BookInfoForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {handleSubmit, reset, resetSection} = this.props;
+
+        return (
+            <fieldset>
+                <legend>图书信息</legend>
+                <form onSubmit={handleSubmit}>
+                    书名：
+                    <Field name="bookName" component="input" type="text"/>
+                    <br/><br/>
+
+                    页号：
+                    <Field name="pageNums" component="input" type="number"/>
+                    <br/><br/>
+
+                    书号：
+                    <Field name="isbn" component="input" type="text"/>
+                    <br/><br/>
+
+
+                    <div>
+                        <button onClick={() => resetSection("bookName")}>清空[书名]</button>
+                        <button onClick={() => resetSection("pageNums")}>清空[页数]</button>
+                        <button onClick={() => resetSection("isbn")}>清空[书号]</button>
+                        <button onClick={() => reset()}>清空全部</button>
+                        <button type="submit">提交</button>
+                    </div>
+                </form>
+            </fieldset>
         );
     }
 }
@@ -54,15 +120,11 @@ class App extends React.Component {
         super(props);
     }
 
-    submit = values => {
-        // print the form values to the console
-        console.log(values)
-    }
-
     render() {
         return (
             <div>
-                <UserInfoForm onSubmit={this.submit}/>
+                <UserInfoForm/>
+                <BookInfoForm/>
             </div>
         );
     }
@@ -75,7 +137,11 @@ const rootReducer = combineReducers({
     form: formReducer, // 挂载redux-form的reducer;
 });
 
-const store = createStore(rootReducer)
+const storeEnhancer = composeWithDevTools(
+    applyMiddleware()
+);
+
+const store = createStore(rootReducer, storeEnhancer);
 
 ReactDOM.render(
     <Provider store={store}>
