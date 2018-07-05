937c4e973d1d2127089f6bc89e093f21d703d784
xukaihong <2371726333@qq.com>
2018-07-05 11:34:20 +0800

    导出Rui

--- 05_sep_v1/src/framework/core/index.js
+++ 05_sep_v1/src/framework/core/index.js
@@ -2,10 +2,12 @@ import {connect} from "react-redux";
 import reaper from "./reaper";
 import dynamic from "./reaper/dynamic";
 import reaperLoading  from "./reaper-loading/index";
+import  Rui from  "./Rui";
 
 export {
     connect,
     reaper,
     dynamic,
     reaperLoading,
+    Rui
 }
