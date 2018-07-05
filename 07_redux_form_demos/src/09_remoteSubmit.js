db366c2779b0d079a45656b56ec6e374c1ac010c
webj2ee <1696627335@qq.com>
2018-07-05 23:48:27 +0800

    Signed-off-by: webj2ee <1696627335@qq.com>

---
+++ 07_redux_form_demos/src/09_remoteSubmit.js
@@ -0,0 +1,138 @@
+/////////////////////////////////////////////////////////////////////////////
+// Dependency
+//
+// ## React
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// ## Redux
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {Provider, connect} from 'react-redux';
+
+// ## Devtools
+import {composeWithDevTools} from "redux-devtools-extension";
+
+// ## redux-form
+import {reducer as reduxFormReducer} from 'redux-form'
+import {Field, reduxForm, submit, SubmissionError} from 'redux-form'
+
+// ## util
+import {delay} from "./utils";
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Form Field
+//
+const rowStyle = {display: "flex", justifyContent: "flex-start", alignItems: "center", marginBottom: 10};
+const labelStyle = {width: 120, textAlign: "right"};
+
+const renderField = ({input, label, type, meta: {touched, error}}) => (
+    <div style={rowStyle}>
+        <label style={labelStyle}>{label}</label>
+        <div>
+            <input {...input} placeholder={label} type={type}/>
+            {touched && error && <span>{error}</span>}
+        </div>
+    </div>
+);
+
+/////////////////////////////////////////////////////////////////////////////
+// Form
+//
+@reduxForm({
+    form: 'remoteSubmit',   // a unique identifier for this form
+    onSubmit: (values) => { // submit function must be passed to onSubmit
+        return delay(1000).then(() => {
+            // simulate server latency
+            if (!['john', 'paul', 'george', 'ringo'].includes(values.username)) {
+                throw new SubmissionError({
+                    username: 'User does not exist',
+                    _error: 'Login failed!'
+                })
+            } else if (values.password !== 'redux-form') {
+                throw new SubmissionError({
+                    password: 'Wrong password',
+                    _error: 'Login failed!'
+                })
+            } else {
+                window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+            }
+        })
+    }
+})
+class RemoteSubmitForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {error, handleSubmit} = this.props;
+        return (
+            <form onSubmit={handleSubmit}>
+                <Field
+                    name="username"
+                    type="text"
+                    component={renderField}
+                    label="Username"
+                />
+                <Field
+                    name="password"
+                    type="password"
+                    component={renderField}
+                    label="Password"
+                />
+                {error && <strong>{error}</strong>}
+                <div>
+                    No submit button in the form. The submit button below is a separate
+                    unlinked component.
+                </div>
+            </form>
+        )
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Start
+//
+@connect()
+class RemoteSubmitButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <button type="button"
+                style={{
+                    padding: '10px 20px',
+                    width: 140,
+                    display: 'block',
+                    margin: '20px auto',
+                    fontSize: '16px'
+                }}
+                onClick={() => this.props.dispatch(submit('remoteSubmit'))}>Submit</button>
+        )
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Start
+//
+const rootReducer = combineReducers({
+    form: reduxFormReducer // mounted under "form"
+});
+
+const storeEnhancer = composeWithDevTools();
+
+const store = createStore(rootReducer, storeEnhancer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <div>
+            <RemoteSubmitForm/>
+            <RemoteSubmitButton/>
+        </div>
+    </Provider>,
+    document.getElementById('app')
+);
