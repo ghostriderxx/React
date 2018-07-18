50748ccbc8fe0fa8d6572e6c941201ab401b81e3
webj2ee <1696627335@qq.com>
2018-07-18 16:45:23 +0800

    观察react-router生命周期；

--- 09_router_demos/src/index.js
+++ 09_router_demos/src/index.js
@@ -34,44 +34,124 @@ const store = createStore(
 ///////////////////////////////////////////////////////////////////////////////////////////
 // Components
 //
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
-);
-const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
-);
-const Topics = ({ match }) => (
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            <li>
-                <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/components`}>Components</Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-            </li>
-        </ul>
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("Home: constructor");
+    }
 
-        <Route path={`${match.url}/:topicId`} component={Topic} />
-        <Route
-            exact
-            path={match.url}
-            render={() => <h3>Please select a topic.</h3>}
-        />
-    </div>
-);
-const Topic = ({ match }) => (
-    <div>
-        <h3>{match.params.topicId}</h3>
-    </div>
-);
+    componentWillMount(){
+        console.log("Home: componentWillMount");
+    }
+
+    componentWillUnmount(){
+        console.log("Home: componentWillUnmount");
+    }
+
+    render() {
+        console.log("Home: render");
+
+        return (
+            <div>
+                <h2>Home</h2>
+            </div>
+        );
+    }
+}
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("About: constructor");
+    }
+
+    componentWillMount(){
+        console.log("About: componentWillMount");
+    }
+
+    componentWillUnmount(){
+        console.log("About: componentWillUnmount");
+    }
+
+    render() {
+        console.log("About: render");
+        return (
+            <div>
+                <h2>About</h2>
+            </div>
+        );
+    }
+}
+
+class Topics extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("Topics: constructor");
+    }
+
+    componentWillMount(){
+        console.log("Topics: componentWillMount");
+    }
+
+    componentWillUnmount(){
+        console.log("Topics: componentWillUnmount");
+    }
+
+    render() {
+        console.log("Topics: render");
+
+        const {match} = this.props;
+        return (
+            <div>
+                <h2>Topics</h2>
+                <ul>
+                    <li>
+                        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+                    </li>
+                    <li>
+                        <Link to={`${match.url}/components`}>Components</Link>
+                    </li>
+                    <li>
+                        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+                    </li>
+                </ul>
+
+                <Route path={`${match.url}/:topicId`} component={Topic}/>
+                <Route
+                    exact
+                    path={match.url}
+                    render={() => <h3>Please select a topic.</h3>}
+                />
+            </div>
+        );
+    }
+}
+
+class Topic extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("Topic: constructor");
+    }
+
+    componentWillMount(){
+        console.log("Topic: componentWillMount");
+    }
+
+    componentWillUnmount(){
+        console.log("Topic: componentWillUnmount");
+    }
+
+    render() {
+        console.log("Topic: render");
+
+        const {match} = this.props;
+        return (
+            <div>
+                <h3>{match.params.topicId}</h3>
+            </div>
+        );
+    }
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 // Start
@@ -81,6 +161,19 @@ ReactDOM.render(
         {/* ConnectedRouter will use the store from Provider automatically */}
         <ConnectedRouter history={history}>
             <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/topics">Topics</Link>
+                    </li>
+                </ul>
+                <hr/>
+
                 <Route exact path="/" component={Home}/>
                 <Route path="/about" component={About}/>
                 <Route path="/topics" component={Topics}/>
