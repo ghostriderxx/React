c6793c9188c0f1cf49ccdabc9018d068717d3288
WEBJ2EE <1696627335@qq.com>
2018-10-28 22:33:33 +0800

    Signed-off-by: WEBJ2EE <1696627335@qq.com>

--- 14_npm_libs/rimraf/index.js
+++ 14_npm_libs/rimraf/index.js
@@ -1,35 +1,44 @@
 const rimraf = require("rimraf");
 const glob = require("glob");
+const globby = require("globby");
 const path = require("path");
 const chalk = require("chalk");
 
 // 包装 rimraf 为 Promise 模式，方便使用
-function del(path){
-    return new Promise((resolve) => rimraf(path, ()=>resolve(path)));
+function del(path) {
+    return new Promise((resolve) => rimraf(path, () => resolve(path)));
 }
 
-// 包装 glob 为 Promise 模式，方便使用
-function dir(pattern){
-    return new Promise((resolve, reject) => {
-        glob(pattern, {
-            cwd: path.resolve(__dirname)
-        }, function(err, matches){
-            resolve(matches);
-        });
-    });
-}
-
-dir("./tmp/*").then((matches)=>{
-    return Promise.all(matches.map((match) => {
-
-        const pathToDelete = path.resolve(__dirname, match);
-
-        return del(pathToDelete);
-    }));
-}).then((files)=>{
+function log(files) {
     console.log(chalk.red.bold(`Files:
 
 ${files.join("\n")}
 
 deleted!`));
-});
+}
+
+/////////////////////////////////////////////////////////////////////
+// Version1: 使用基础版 glob
+//
+function dir(pattern) {
+    return new Promise((resolve, reject) => {
+        glob(pattern, {
+            cwd: path.resolve(__dirname)
+        }, function (err, matches) {
+            resolve(matches);
+        });
+    });
+}
+
+dir("./tmp/*").then((matches) => {
+    return Promise.all(matches.map((match) => del(path.resolve(__dirname, match))));
+}).then((files) => log(files));
+
+/////////////////////////////////////////////////////////////////////
+// Version2: 使用增强版 globby
+//
+globby("./tmp/*", {
+    cwd: path.resolve(__dirname)
+}).then((matches) => {
+    return Promise.all(matches.map((match) => del(path.resolve(__dirname, match))));
+}).then((files) => log(files));
