d8e175b6501a483b1983fa0c8f5ba514606c83e8
WEBJ2EE <1696627335@qq.com>
2018-10-28 17:24:33 +0800

    Signed-off-by: WEBJ2EE <1696627335@qq.com>

--- 14_npm_libs/rimraf/index.js
+++ 14_npm_libs/rimraf/index.js
@@ -1,12 +1,12 @@
 const rimraf = require("rimraf");
 const path = require("path");
 
-function delTmp(){
-    const tmpPath = path.resolve(__dirname, "./tmp/");
-
-    new Promise((resolve) => rimraf(tmpPath, resolve)).then(()=>{
-        console.log(`Del ${tmpPath} success!`);
-    });
+function del(path){
+    return new Promise((resolve) => rimraf(tmpPath, resolve));
 }
 
-delTmp();
+const tmpPath = path.resolve(__dirname, "./tmp/");
+
+del(tmpPath).then(()=>{
+    console.log(`Del ${tmpPath} success!`);
+});
