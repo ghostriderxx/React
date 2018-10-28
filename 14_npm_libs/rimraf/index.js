10f0279f5785b07e48827803683137a42ef6bf6a
WEBJ2EE <1696627335@qq.com>
2018-10-28 17:49:38 +0800

    Signed-off-by: WEBJ2EE <1696627335@qq.com>

--- 14_npm_libs/rimraf/index.js
+++ 14_npm_libs/rimraf/index.js
@@ -1,10 +1,12 @@
 const rimraf = require("rimraf");
 const path = require("path");
 
+// 包装 rimraf 为 Promise 模式，方便使用
 function del(path){
     return new Promise((resolve) => rimraf(path, resolve));
 }
 
+// 删除 tmp 目录下的所有文件
 const tmpPath = path.resolve(__dirname, "./tmp/*");
 
 del(tmpPath).then(()=>{
