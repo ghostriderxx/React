d3480a8b422c6bca45cb39b76ac2b93e3b8d14ea
lzx <lzx@dareway.com>
2018-11-02 14:36:45 +0800

    Signed-off-by: lzx <lzx@dareway.com>

--- 14_npm_libs/parse5/index.js
+++ 14_npm_libs/parse5/index.js
@@ -3,12 +3,74 @@ const parse5 = require("parse5");
 const path = require("path");
 const assert = require('assert');
 
+/**
+ * AbstractNode：
+ *
+ *  export interface AbstractNode {
+ *      tag: string;
+ *      attrs: {
+ *          [key: string]: string;
+ *      };
+ *      children?: AbstractNode[];
+ *  }
+ *
+ */
+function normalizeNode(node){
+    // 分离 tagName
+    const tag = node.tagName;
+    if (!tag) {
+        throw new TypeError(`Element should have no no-tag node`);
+    }
+
+    // 分离 attrs
+    const attrs = node.attrs.reduce((acc, { name, value }) => {
+        Object.defineProperty(acc, name, { value, enumerable: true });
+        return acc;
+    }, {});
+
+    // 分离 childNodes
+    const children = node.childNodes.map((child) =>
+        normalizeNode(child)
+    );
+    const result = {
+        tag,
+        attrs
+    };
+    if (children.length) {
+        result.children = children;
+    }
+    return result;
+}
+
 function generateAbstractTree(node){
-    assert(node);
+    // 要求函数入参为 svg 节点
     assert(node.tagName === 'svg');
 
+    // svg 节点必须包含 viewBox 属性
     const viewBox = node.attrs.find(({ name }) => name === 'viewBox');
-    assert(viewBox, "标签 svg 中不存在 viewBox 属性!");
+    assert(viewBox, "svg 节点中必须包含 viewBox 属性!");
+
+    // viewBox 属性值必须满足 x y width height 格式
+    const size =  viewBox.value
+        .split(' ')
+        .slice(2)
+        .map((str) => Number.parseInt(str, 10));
+    assert(
+        size.length === 2,
+        `The size tuple should be [ width, height ], but got [ ${size[0]}, ${
+            size[1]
+            } ]`
+    );
+
+    // svg 下至多能存在一个 path 子节点
+    const oneLevelPathNodes = node.childNodes.filter(
+        ({ nodeName, childNodes }) =>
+            nodeName !== 'style' && childNodes.length === 0
+    );
+    assert(oneLevelPathNodes.length >= 1, "svg 下至多能存在一个 path 子节点");
+
+    // 转换为 AbstractNode
+    return normalizeNode(node);
 }
 
 async function run(){
@@ -20,7 +82,8 @@ async function run(){
     const docFragment = parse5.parseFragment(svgData);
 
     // 遍历
-    generateAbstractTree(docFragment.childNodes[0]);
+    const tree = generateAbstractTree(docFragment.childNodes[0]);
+    console.log(JSON.stringify(tree));
 }
 
 run();
