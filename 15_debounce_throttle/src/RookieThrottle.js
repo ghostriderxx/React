f4ab463b0040c50d792d27ca3294809c2e4599ad
WEBJ2EE <1696627335@qq.com>
2018-11-06 20:09:18 +0800

    Signed-off-by: WEBJ2EE <1696627335@qq.com>

--- 15_debounce_throttle/src/RookieThrottle.js
+++ 15_debounce_throttle/src/RookieThrottle.js
@@ -1,3 +1,12 @@
 export default function RookieThrottle(func, wait){
+    let lastTime = Date.now();
 
+    return function(...args){
+        const currentTime = Date.now();
+        if(currentTime - lastTime < wait){
+            return;
+        }
+        lastTime = Date.now();
+        func(...args);
+    }
 }
