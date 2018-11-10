9a3fe834029b865ed538af6e1002ac697afe32e6
WEBJ2EE <1696627335@qq.com>
2018-11-11 01:18:45 +0800

    Signed-off-by: WEBJ2EE <1696627335@qq.com>

--- 16_ant-design-icons-no-typescript/packages/icons/build/utils/normalizeNames/index.js
+++ 16_ant-design-icons-no-typescript/packages/icons/build/utils/normalizeNames/index.js
@@ -6,11 +6,34 @@ import { normalizeNamesFromDir } from './helpers';
 
 const folderNames = ['fill', 'outline', 'twotone'];
 export async function normalize(env) {
+
+    /**
+     * 修正以下目录中的 svg 文件的文件名：
+     *      1. svg/fill
+     *      2. svg/outline
+     *      3. svg/twotone
+     * 
+     * 修正规则：
+     *      1. 删除 -o 后缀;
+     *      2. 删除 -fill 后缀;
+     *      3. 删除 -twotone 后缀;
+     *      4. 按映射规则调整一部分 svg 文件的文件名;
+     *             build/constants.js
+     */
     for (const folderName of folderNames) {
         const dir = path.join(env.paths.SVG_DIR, folderName);
         await normalizeNamesFromDir(dir);
         log.notice(`Normalize ${dir}`);
     }
+
+    /**
+     * 将以下目录： 
+     *      svg/fill
+     *      svg/outline
+     *      svg/twotone 
+     * 中所有 svg 文件的文件名
+     * 去除扩展名、去重、汇总
+     */
     const listNames = _.uniq(
         _.flatten(
             await Promise.all(
