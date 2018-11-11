9c054115080e188a8b1ad3e33ea5f2f7a601bbde
WEBJ2EE <1696627335@qq.com>
2018-11-11 15:08:10 +0800

    Signed-off-by: WEBJ2EE <1696627335@qq.com>

--- 16_ant-design-icons-no-typescript/packages/icons/build/generateIcons.js
+++ 16_ant-design-icons-no-typescript/packages/icons/build/generateIcons.js
@@ -143,249 +143,201 @@ export async function build(env) {
         )
     );
     
-    // SVG Meta Data Flow
-    const svgMetaDataWithTheme$ = from(['fill', 'outline', 'twotone']).pipe(
-        map((theme) =>
-            from(svgBasicNames).pipe(
-                map((kebabCaseName) => {
-                    const identifier = getIdentifier(_.upperFirst(_.camelCase(kebabCaseName)), theme);
-                    return {kebabCaseName, identifier};
-                }),
-                filter(({kebabCaseName}) =>
-                    isAccessable(path.resolve(env.paths.SVG_DIR, theme, `${kebabCaseName}.svg`))
-                ),
-                mergeMap(async ({kebabCaseName, identifier}) => {
-                    const tryUrl = path.resolve(env.paths.SVG_DIR, theme, `${kebabCaseName}.svg`);
-                    let optimizer = svgo;
-                    if (singleType.includes(theme)) {
-                        optimizer = svgoForSingleIcon;
-                    }
-                    const {data} = await optimizer.optimize(await fs.readFile(tryUrl, 'utf8'));
-                    const icon = {
-                        name: kebabCaseName,
-                        theme,
-                        icon: {
-                            ...generateAbstractTree(parse5.parseFragment(data).childNodes[0], kebabCaseName)
-                        }
-                    };
-                    return {identifier, icon};
-                })
-            )
-        )
-    );
-
-    svgMetaDataWithTheme$.subscribe((metaData$) => (metaData$.subscribe((data) => console.log(data))))
-    return;
-
-
-    // Nomalized build time icon meta data
-    const BuildTimeIconMetaData$ = svgMetaDataWithTheme$.pipe(
-        mergeMap((metaData$) => metaData$),
-        map(({identifier, icon}) => {
-            icon = _.cloneDeep(icon);
-            if (typeof icon.icon !== 'function') {
-                if (!oldIcons.includes(icon.name)) {
-                    icon.icon.attrs.viewBox = '64 64 896 896';
-                }
-                if (icon.icon.attrs.class) {
-                    icon.icon.attrs = _.omit(icon.icon.attrs, ['class']);
-                }
+    /**
+     * 调整 BuildTimeIconMetaData 中的属性
+     * 
+     * 1. viewBox
+     * 2. class
+     * 3. fill
+     */
+    const buildTimeIconMetaData = svgMetaDataWithTheme.map(({identifier, icon}) => {
+        icon = _.cloneDeep(icon);
+        if (typeof icon.icon !== 'function') {
+            if (!oldIcons.includes(icon.name)) {
+                icon.icon.attrs.viewBox = '64 64 896 896';
             }
-            if (icon.theme === 'twotone') {
-                if (typeof icon.icon !== 'function' && icon.icon.children) {
-                    icon.icon.children.forEach((pathElment) => {
-                        pathElment.attrs.fill = pathElment.attrs.fill || '#333';
-                    });
-                }
+            if (icon.icon.attrs.class) {
+                icon.icon.attrs = _.omit(icon.icon.attrs, ['class']);
             }
-            return {identifier, icon};
-        })
-    );
-
-    // Inline SVG files content flow.
-    const inlineSVGFiles$ = BuildTimeIconMetaData$.pipe(
-        map(({icon}) => {
-            return {
-                path: path.resolve(env.paths.INLINE_SVG_OUTPUT_DIR, icon.theme, `./${icon.name}.svg`),
-                content: renderIconDefinitionToSVGElement(icon)
-            };
-        })
-    );
-
-    // Icon files content flow.
-    const iconTsTemplate = await fs.readFile(env.paths.ICON_TEMPLATE, 'utf8');
-    const iconFiles$ = BuildTimeIconMetaData$.pipe(
-        map(({identifier, icon}) => {
-            return {
-                identifier,
-                theme: icon.theme,
-                content:
-                    icon.theme === 'twotone'
-                        ? Prettier.format(
-                        iconTsTemplate
-                            .replace(ICON_IDENTIFIER, identifier)
-                            .replace(
-                                ICON_JSON,
-                                JSON.stringify({...icon, icon: 'FUNCTION'}).replace(
-                                    `"FUNCTION"`,
-                                    `function (primaryColor: string, secondaryColor: string) {` +
-                                    ` return ${replaceFillColor(JSON.stringify(icon.icon))};` +
-                                    ` }`
-                                )
-                            ),
-                        {...env.options.prettier, parser: 'babylon'}
-                        )
-                        : Prettier.format(
-                        iconTsTemplate
-                            .replace(ICON_IDENTIFIER, identifier)
-                            .replace(ICON_JSON, JSON.stringify(icon)),
-                        env.options.prettier
-                        )
-            };
-        }),
-        map(({identifier, content, theme}) => ({
-            path: path.resolve(env.paths.ICON_OUTPUT_DIR, theme, `./${identifier}.js`),
-            content
-        }))
-    );
-
-    // Index File content flow
-    const indexTsTemplate = await fs.readFile(env.paths.INDEX_TEMPLATE, 'utf8');
-    const indexFile$ = svgMetaDataWithTheme$.pipe(
-        mergeMap((metaData$) => metaData$),
-        reduce(
-            (acc, {identifier, icon}) =>
-                acc + `export { default as ${identifier} } from './${icon.theme}/${identifier}';\n`,
-            ''
-        ),
-        map((content) => ({
-            path: env.paths.INDEX_OUTPUT,
-            content: Prettier.format(
-                indexTsTemplate.replace(EXPORT_DEFAULT_COMPONENT_FROM_DIR, content),
-                env.options.prettier
-            )
-        }))
-    );
-
-    // Manifest File content flow
-    const manifestTsTemplate = await fs.readFile(env.paths.MANIFEST_TEMPLATE, 'utf8');
-    const manifestFile$ = from(['fill', 'outline', 'twotone']).pipe(
-        map((theme) => ({
-            theme,
-            names: svgBasicNames.filter((name) => isAccessable(path.resolve(env.paths.SVG_DIR, theme, `${name}.svg`)))
-        })),
-        reduce(
-            (acc, {theme, names}) => {
-                acc[theme] = names;
-                return acc;
-            },
-            {fill: [], outline: [], twotone: []}
-        ),
-        map((names) => ({
-            path: env.paths.MANIFEST_OUTPUT,
-            content: Prettier.format(
-                manifestTsTemplate.replace(EXPORT_DEFAULT_MANIFEST, `export default ${JSON.stringify(names)};`),
-                env.options.prettier
-            )
-        }))
-    );
-
-    // Dist File content flow
-    const distTsTemplate = await fs.readFile(env.paths.DIST_TEMPLATE, 'utf8');
-    const dist$ = BuildTimeIconMetaData$.pipe(
-        map(({identifier, icon}) => {
-            let content = '';
-            if (icon.theme === 'twotone') {
-                if (typeof icon.icon !== 'function') {
-                    const paths = (icon.icon.children || [])
-                        .filter(({attrs}) => typeof attrs.d === 'string') // fix the <defs> element in the top level children
-                        .map(({attrs}) => {
-                            const {fill, d} = attrs;
-                            if (fill && d) {
-                                return `['${fill}', '${d}']`;
-                            }
-                            return `'${d}'`;
-                        })
-                        .join(',');
-                    content = Prettier.format(
-                        `export const ${identifier} = ` +
-                        `getIcon('${icon.name}', '${icon.theme}', ${replaceFillColor(
-                            `function (primaryColor, secondaryColor) {` +
-                            ` return getNode('${icon.icon.attrs.viewBox}', ${paths}) }`
-                        )})`,
-                        {...env.options.prettier, parser: 'babylon'}
-                    );
-                }
-            } else {
-                if (typeof icon.icon !== 'function') {
-                    const paths = (icon.icon.children || [])
-                        .filter(({attrs}) => typeof attrs.d === 'string')
-                        .map(({attrs}) => {
-                            const {fill, d} = attrs;
-                            if (fill && d) {
-                                return `['${fill}', '${d}']`;
-                            }
-                            return `'${d}'`;
-                        })
-                        .join(',');
-                    content = Prettier.format(
-                        `export const ${identifier} = ` +
-                        `getIcon('${icon.name}', '${icon.theme}', ` +
-                        `getNode('${icon.icon.attrs.viewBox}', ${paths})` +
-                        `);`,
-                        env.options.prettier
-                    );
-                }
+        }
+        if (icon.theme === 'twotone') {
+            if (typeof icon.icon !== 'function' && icon.icon.children) {
+                icon.icon.children.forEach((pathElment) => {
+                    pathElment.attrs.fill = pathElment.attrs.fill || '#333';
+                });
             }
-            content = content
-                .replace(NORMAL_VIEWBOX, 'normalViewBox')
-                .replace(NEW_VIEWBOX, 'newViewBox')
-                .replace(THEME_FILL, 'fill')
-                .replace(THEME_OUTLINE, 'outline')
-                .replace(THEME_TWOTONE, 'twotone');
-            return content;
-        }),
-        reduce((acc, nextContent) => acc + nextContent, ''),
-        map((content) => ({
-            path: env.paths.DIST_OUTPUT,
-            content: distTsTemplate.replace(EXPORT_DIST, content)
-        }))
-    );
-
-    // // Types file content flow
-    // const typesTsTemplate = await fs.readFile(env.paths.TYPES_TEMPLATE, 'utf8');
-    // const types$ = of({
-    //     path: env.paths.TYPES_OUTPUT,
-    //     content: typesTsTemplate
-    // });
-
-    // Helpers file content flow
-    const helpersTsTemplate = await fs.readFile(env.paths.HELPERS_TEMPLATE, 'utf8');
-    const helpers$ = of({
-        path: env.paths.HELPERS_OUTPUT,
-        content: helpersTsTemplate
+        }
+        return {identifier, icon};
     });
 
-    const files$ = iconFiles$.pipe(
-        concat(inlineSVGFiles$),
-        concat(manifestFile$),
-        concat(indexFile$),
-        concat(dist$),
-        // concat(types$),
-        concat(helpers$)
-    );
+    
+    /**
+     * 生成 inline-svg 的 WriteFileMetaData
+     * 
+     * export interface WriteFileMetaData {
+     *      path: string;
+     *      content: string;
+     * }
+     */
+    const inlineSVGFiles = buildTimeIconMetaData.map(({icon}) => {
+        return {
+            path: path.resolve(env.paths.INLINE_SVG_OUTPUT_DIR, icon.theme, `./${icon.name}.svg`),
+            content: renderIconDefinitionToSVGElement(icon)
+        };
+    });
 
-    return new Promise((resolve, reject) => {
-        const subscription = files$
-            .pipe(
-                mergeMap(async ({path: writeFilePath, content}) => {
-                    await fs.writeFile(writeFilePath, content, 'utf8');
-                    log.info(`Generated ./${path.relative(env.base, writeFilePath)}`);
-                })
-            )
-            .subscribe(undefined, reject, () => {
-                log.notice('Done.');
-                resolve(subscription);
-            });
+    /**
+     * 生成 src/icon/[theme]/ 下文件的 WriteFileMetaData
+     */
+    const iconTsTemplate = await fs.readFile(env.paths.ICON_TEMPLATE, 'utf8');
+    const iconFiles$ = buildTimeIconMetaData.map(({identifier, icon}) => {
+        return {
+            identifier,
+            theme: icon.theme,
+            content:
+                icon.theme === 'twotone'
+                    ? Prettier.format(
+                    iconTsTemplate
+                        .replace(ICON_IDENTIFIER, identifier)
+                        .replace(
+                            ICON_JSON,
+                            JSON.stringify({...icon, icon: 'FUNCTION'}).replace(
+                                `"FUNCTION"`,
+                                `function (primaryColor: string, secondaryColor: string) {` +
+                                ` return ${replaceFillColor(JSON.stringify(icon.icon))};` +
+                                ` }`
+                            )
+                        ),
+                    {...env.options.prettier, parser: 'babylon'}
+                    )
+                    : Prettier.format(
+                    iconTsTemplate
+                        .replace(ICON_IDENTIFIER, identifier)
+                        .replace(ICON_JSON, JSON.stringify(icon)),
+                    env.options.prettier
+                    )
+        };
+    }).map(({identifier, content, theme}) => ({
+        path: path.resolve(env.paths.ICON_OUTPUT_DIR, theme, `./${identifier}.js`),
+        content
+    }));
+
+    /**
+     * 生成 src/index.js 的 WriteFileMetaData
+     */
+    const indexTsTemplate = await fs.readFile(env.paths.INDEX_TEMPLATE, 'utf8');
+    const indexFileContent = svgMetaDataWithTheme.reduce(
+        (acc, {identifier, icon}) =>
+            acc + `export { default as ${identifier} } from './${icon.theme}/${identifier}';\n`,
+        ''
+    );
+    const indexFile = {
+        path: env.paths.INDEX_OUTPUT,
+        content: Prettier.format(
+            indexTsTemplate.replace(EXPORT_DEFAULT_COMPONENT_FROM_DIR, indexFileContent),
+            env.options.prettier
+        )
+    };
+
+    /**
+     * 生成 src/manifest.js 的 WriteFileMetaData
+     */
+    const manifestTsTemplate = await fs.readFile(env.paths.MANIFEST_TEMPLATE, 'utf8');
+    const manifestFileContent = ['fill', 'outline', 'twotone'].map((theme) => ({
+        theme,
+        names: svgBasicNames.filter((name) => isAccessable(path.resolve(env.paths.SVG_DIR, theme, `${name}.svg`)))
+    })).reduce(
+        (acc, {theme, names}) => {
+            acc[theme] = names;
+            return acc;
+        },
+        {fill: [], outline: [], twotone: []}
+    );
+    const manifestFile = {
+        path: env.paths.MANIFEST_OUTPUT,
+        content: Prettier.format(
+            manifestTsTemplate.replace(EXPORT_DEFAULT_MANIFEST, `export default ${JSON.stringify(manifestFileContent)};`),
+            env.options.prettier
+        )
+    };
+
+    /**
+     * 生成 src/dist.js 的 WriteFileMetaData
+     */
+    const distTsTemplate = await fs.readFile(env.paths.DIST_TEMPLATE, 'utf8');
+    const distContent = buildTimeIconMetaData.map(({identifier, icon}) => {
+        let content = '';
+        if (icon.theme === 'twotone') {
+            if (typeof icon.icon !== 'function') {
+                const paths = (icon.icon.children || [])
+                    .filter(({attrs}) => typeof attrs.d === 'string') // fix the <defs> element in the top level children
+                    .map(({attrs}) => {
+                        const {fill, d} = attrs;
+                        if (fill && d) {
+                            return `['${fill}', '${d}']`;
+                        }
+                        return `'${d}'`;
+                    })
+                    .join(',');
+                content = Prettier.format(
+                    `export const ${identifier} = ` +
+                    `getIcon('${icon.name}', '${icon.theme}', ${replaceFillColor(
+                        `function (primaryColor, secondaryColor) {` +
+                        ` return getNode('${icon.icon.attrs.viewBox}', ${paths}) }`
+                    )})`,
+                    {...env.options.prettier, parser: 'babylon'}
+                );
+            }
+        } else {
+            if (typeof icon.icon !== 'function') {
+                const paths = (icon.icon.children || [])
+                    .filter(({attrs}) => typeof attrs.d === 'string')
+                    .map(({attrs}) => {
+                        const {fill, d} = attrs;
+                        if (fill && d) {
+                            return `['${fill}', '${d}']`;
+                        }
+                        return `'${d}'`;
+                    })
+                    .join(',');
+                content = Prettier.format(
+                    `export const ${identifier} = ` +
+                    `getIcon('${icon.name}', '${icon.theme}', ` +
+                    `getNode('${icon.icon.attrs.viewBox}', ${paths})` +
+                    `);`,
+                    env.options.prettier
+                );
+            }
+        }
+        content = content
+            .replace(NORMAL_VIEWBOX, 'normalViewBox')
+            .replace(NEW_VIEWBOX, 'newViewBox')
+            .replace(THEME_FILL, 'fill')
+            .replace(THEME_OUTLINE, 'outline')
+            .replace(THEME_TWOTONE, 'twotone');
+        return content;
+    }).reduce((acc, nextContent) => acc + nextContent, '');
+    const dist = {
+        path: env.paths.DIST_OUTPUT,
+        content: distTsTemplate.replace(EXPORT_DIST, distContent)
+    };
+
+    /**
+     * 生成 src/helpers.js 的 WriteFileMetaData
+     */
+    const helpersTsTemplate = await fs.readFile(env.paths.HELPERS_TEMPLATE, 'utf8');
+    const helpers = {
+        path: env.paths.HELPERS_OUTPUT,
+        content: helpersTsTemplate
+    };
+
+    /**
+     * 批量写文件
+     */
+    return Promise.all(_.flatten([inlineSVGFiles, manifestFile, indexFile, dist, helpers]).map(async ({path: writeFilePath, content}) => {
+        await fs.writeFile(writeFilePath, content, 'utf8');
+        log.info(`Generated ./${path.relative(env.base, writeFilePath)}`);
+    })).then(()=>{
+        log.notice('Done.');
     });
 }
