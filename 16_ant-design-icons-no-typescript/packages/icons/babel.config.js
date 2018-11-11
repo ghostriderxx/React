3395a17485ba7c91c5c745f3546ba8cca913dd0a
WEBJ2EE <1696627335@qq.com>
2018-11-11 16:46:36 +0800

    代码结构向antd靠拢

--- 16_ant-design-icons-no-typescript/packages/icons/babel.config.js
+++ 16_ant-design-icons-no-typescript/packages/icons/babel.config.js
@@ -1,11 +1,31 @@
 module.exports = {
-    presets: ['@babel/env', '@babel/react'],
-    plugins: [
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-syntax-dynamic-import',
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        '@babel/plugin-syntax-class-properties',
-        '@babel/plugin-syntax-object-rest-spread',
-        '@babel/plugin-transform-runtime'
-    ]
-};
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: false,
+        useBuiltIns: "usage",
+        shippedProposals: true,
+        targets: {
+          browsers: [
+            '> 1%',
+            'last 2 versions',
+            'not ie <= 8'
+          ]
+        }
+      },
+    ],
+    [
+      "@babel/preset-react",
+      {
+        useBuiltIns: true,
+        pragma: "React.createElement",
+      },
+    ],
+  ],
+  plugins: [
+    ['@babel/plugin-transform-runtime'],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties']
+  ],
+}
