0fc7467eb9b8bfe79b97c4f12a7635e122274343
WEBJ2EE <1696627335@qq.com>
2018-11-11 16:59:57 +0800

    Signed-off-by: WEBJ2EE <1696627335@qq.com>

--- 16_ant-design-icons-no-typescript/packages/icons/babel.config.js
+++ 16_ant-design-icons-no-typescript/packages/icons/babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    presets: ['@babel/env',
+    presets: [
+        [
+            "@babel/preset-env",
+        ],
         [
             "@babel/preset-react",
             {
